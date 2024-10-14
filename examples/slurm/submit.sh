6717a318f4154736eda3b13427d2cada8425302a
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2024-10-13 20:01:54 -0400

    Fix: add args to run models w/ optional name for faster iterating

--- examples/slurm/submit.sh
+++ examples/slurm/submit.sh
@@ -24,17 +24,66 @@ module load anaconda3/2022.10
 conda activate /ocean/projects/bio210019p/stevesho/ogl
 
 
-configs=(k562_allloopshicfdr_global.yaml)
+# BEST PERFORMING
+# /ocean/projects/bio210019p/stevesho/data/preprocess/graph_processing/models/regulatory_only_k562_allloopshicfdr_global_GAT_smoothval
+
+
+configs=(all_celllines_alloopshicfdr.yaml)
 for config in "${configs[@]}"; do
   python ogl/omics_graph_learning/ogl_pipeline.py \
     --partition RM \
     --experiment_yaml ogl/configs/experiments/"${config}" \
     --target rna_seq \
     --model GAT \
-    --gnn_layers 3 \
+    --gnn_layers 2 \
     --linear_layers 2 \
     --activation gelu \
-    --dimensions 128 \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.5 \
+    --residual distinct_source \
+    --heads 4 \
+    --positional_encoding \
+    --model_name regulatory_only_all_celllines_alloopshicfdr_gat_2layers_200dim_4attnheads_highdrop
+done
+
+
+configs=(all_celllines_alloopshicfdr.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model GAT \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.5 \
+    --residual distinct_source \
+    --positional_encoding \
+    --attention_task_head \
+    --model_name regulatory_only_all_celllines_alloopshicfdr_gat_2layers_200dim_attntask_highdrop
+done
+
+configs=(k562_allloopshicfdr_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model UniMPTransformer \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
     --batch_size 64 \
     --learning_rate 0.0005 \
     --optimizer AdamW \
@@ -43,7 +92,52 @@ for config in "${configs[@]}"; do
     --residual distinct_source \
     --heads 2 \
     --positional_encoding \
-    --model_name regulatory_only_k562_allloopshicfdr_global_gat_3layers_128dim
+    --model_name regulatory_only_k562_allloopshicfdr_global_UniMPTransformer_2layers_200dim
+done
+
+configs=(k562_allcontacts_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model UniMPTransformer \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.3 \
+    --residual distinct_source \
+    --heads 2 \
+    --positional_encoding \
+    --model_name regulatory_only_k562_allcontacts_global_UniMPTransformer_2layers_200dim
+done
+
+configs=(k562_allloopshicfdr_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model GAT \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.3 \
+    --residual distinct_source \
+    --heads 2 \
+    --positional_encoding \
+    --attention_task_head \
+    --model_name regulatory_only_k562_allloopshicfdr_global_gat_2layers_200dim_attntask
 done
 
 configs=(k562_allcontacts_global.yaml)
@@ -53,10 +147,10 @@ for config in "${configs[@]}"; do
     --experiment_yaml ogl/configs/experiments/"${config}" \
     --target rna_seq \
     --model GAT \
-    --gnn_layers 3 \
+    --gnn_layers 2 \
     --linear_layers 2 \
     --activation gelu \
-    --dimensions 128 \
+    --dimensions 200 \
     --batch_size 64 \
     --learning_rate 0.0005 \
     --optimizer AdamW \
@@ -65,8 +159,142 @@ for config in "${configs[@]}"; do
     --residual distinct_source \
     --heads 2 \
     --positional_encoding \
-    --model_name regulatory_only_k562_allcontacts_global_gat_3layers_128dim
+    --attention_task_head \
+    --model_name regulatory_only_k562_allcontacts_global_gat_2layers_200dim_attntask
 done
+
+configs=(k562_allloopshicfdr_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model GAT \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.3 \
+    --residual distinct_source \
+    --heads 4 \
+    --positional_encoding \
+    --model_name regulatory_only_k562_allloopshicfdr_global_gat_2layers_200dim_4attnheads
+done
+
+configs=(k562_allcontacts_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model GAT \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.3 \
+    --residual distinct_source \
+    --heads 4 \
+    --positional_encoding \
+    --model_name regulatory_only_k562_allcontacts_global_gat_2layers_200dim_4attnheads
+done
+
+configs=(k562_allloopshicfdr_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model GAT \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.5 \
+    --residual distinct_source \
+    --heads 2 \
+    --positional_encoding \
+    --model_name regulatory_only_k562_allloopshicfdr_global_gat_2layers_200dim_highdrop
+done
+
+configs=(k562_allcontacts_global.yaml)
+for config in "${configs[@]}"; do
+  python ogl/omics_graph_learning/ogl_pipeline.py \
+    --partition RM \
+    --experiment_yaml ogl/configs/experiments/"${config}" \
+    --target rna_seq \
+    --model GAT \
+    --gnn_layers 2 \
+    --linear_layers 2 \
+    --activation gelu \
+    --dimensions 200 \
+    --batch_size 64 \
+    --learning_rate 0.0005 \
+    --optimizer AdamW \
+    --scheduler cosine \
+    --dropout 0.5 \
+    --residual distinct_source \
+    --heads 2 \
+    --positional_encoding \
+    --model_name regulatory_only_k562_allcontacts_global_gat_2layers_200dim_highdrop
+done
+
+
+# configs=(k562_allloopshicfdr_global.yaml)
+# for config in "${configs[@]}"; do
+#   python ogl/omics_graph_learning/ogl_pipeline.py \
+#     --partition RM \
+#     --experiment_yaml ogl/configs/experiments/"${config}" \
+#     --target rna_seq \
+#     --model GAT \
+#     --gnn_layers 3 \
+#     --linear_layers 2 \
+#     --activation gelu \
+#     --dimensions 128 \
+#     --batch_size 64 \
+#     --learning_rate 0.0005 \
+#     --optimizer AdamW \
+#     --scheduler cosine \
+#     --dropout 0.3 \
+#     --residual distinct_source \
+#     --heads 2 \
+#     --positional_encoding \
+#     --model_name regulatory_only_k562_allloopshicfdr_global_gat_3layers_128dim
+# done
+
+# configs=(k562_allcontacts_global.yaml)
+# for config in "${configs[@]}"; do
+#   python ogl/omics_graph_learning/ogl_pipeline.py \
+#     --partition RM \
+#     --experiment_yaml ogl/configs/experiments/"${config}" \
+#     --target rna_seq \
+#     --model GAT \
+#     --gnn_layers 3 \
+#     --linear_layers 2 \
+#     --activation gelu \
+#     --dimensions 128 \
+#     --batch_size 64 \
+#     --learning_rate 0.0005 \
+#     --optimizer AdamW \
+#     --scheduler cosine \
+#     --dropout 0.3 \
+#     --residual distinct_source \
+#     --heads 2 \
+#     --positional_encoding \
+#     --model_name regulatory_only_k562_allcontacts_global_gat_3layers_128dim
+# done
 
 
 # configs=(all_celllines_alloopshicfdr.yaml)
